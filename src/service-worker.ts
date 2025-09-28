279219908f53b6e0c9bfc109230875374b51bd86
Vadim Budarin <budarin-vv@yandex.ru>
2025-09-28 22:19:13 +0300

    Add service worker constants and event definitions

---
+++ src/service-worker.ts
@@ -0,0 +1,29 @@
+// app consts
+export const PAGE_RELOAD = 'PAGE_RELOAD';
+export const SKIP_WAITING = 'SKIP_WAITING';
+
+export const SW_INSTALLED = 'SW_INSTALLED';
+export const SW_INSTALLING = 'SW_INSTALLING';
+export const SW_START_INSTALLING = 'SW_START_INSTALLING';
+
+// events
+export const SW_EVENT_INSTALL = 'install';
+export const SW_EVENT_ACTIVATE = 'activate';
+export const SW_EVENT_FETCH = 'fetch';
+export const SW_EVENT_MESSAGE = 'message';
+export const SW_EVENT_SYNC = 'sync';
+export const SW_EVENT_PERIODICSYNC = 'periodicsync';
+export const SW_EVENT_PUSH = 'push';
+export const SW_EVENT_ERROR = 'error';
+export const SW_EVENT_MESSAGEERROR = 'messageerror';
+export const SW_EVENT_UNHANDLEDREJECTION = 'unhandledrejection';
+export const SW_EVENT_REJECTIONHANDLED = 'rejectionhandled';
+export const SW_EVENT_STATECHANGE = 'statechange';
+
+// states
+export const SW_STATE_ACTIVATED = 'activated';
+export const SW_STATE_ACTIVATING = 'activating';
+export const SW_STATE_INSTALLED = 'installed';
+export const SW_STATE_INSTALLING = 'installing';
+export const SW_STATE_PARSED = 'parsed';
+export const SW_STATE_REDUNDANT = 'redundant';
